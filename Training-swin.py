d731c9356dbb4407fa07a16b14d6d078a71d9d0a
Tanveer Hussain <tanveerkhattak3797@gmail.com>
2021-05-11 22:53:06 +0900

    Updates

--- Training-swin.py
+++ Training-swin.py
@@ -17,8 +17,8 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 
-# def count_parameters(model):
-#     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 # def main():
 
