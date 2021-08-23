9c0bf3a3352de704c81329adb0994d42b0e2505b
Tanveer Hussain <tanveerkhattak3797@gmail.com>
2021-08-23 22:35:43 +0900

    Update io.py

--- model_dpt/util/io.py
+++ model_dpt/util/io.py
@@ -111,7 +111,6 @@ def read_image(path):
     img = cv2.imread(path)
 
     if img.ndim == 2:
-    #     print ('Kess than 2 Dimension here')
         img = cv2.cvtColor(img, cv2.COLOR_BGRGRAY)
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) / 255.0
