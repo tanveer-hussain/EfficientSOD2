3eef40f8f1c24078197fb7da4f6e6b9cb7b458a0
tinu <tanveerkhattak3797@gmail.com>
2021-08-27 21:43:33 +0900

    updates

--- ResNet_models.py
+++ ResNet_models.py
@@ -105,9 +105,9 @@ class Encoder_x(nn.Module):
         # mu = (mu - mu_mean) / mu_std
 
         logvar = self.fc2(swin_output)
-        # logvar_mean = torch.mean(logvar, 0, keepdim=True)
-        # log_std = torch.std(logvar, 0, keepdim=True)
-        # logvar = (logvar - logvar_mean) / log_std
+        logvar_mean = torch.mean(logvar, 0, keepdim=True)
+        log_std = torch.std(logvar, 0, keepdim=True)
+        logvar = (logvar - logvar_mean) / log_std
 
         dist = Independent(Normal(loc=mu, scale=torch.exp(logvar)), 1)
         # print(output.size())
