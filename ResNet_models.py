20f6fda1c8eaf411e023a74d92998137a198aa78
tinu <tanveerkhattak3797@gmail.com>
2021-08-27 22:29:58 +0900

    updates

--- ResNet_models.py
+++ ResNet_models.py
@@ -100,9 +100,9 @@ class Encoder_x(nn.Module):
         # output = self.tanh(output)
 
         mu = self.fc1(swin_output)
-        mu_mean = torch.mean(mu, 0, keepdim=True)
-        mu_std = torch.std(mu, 0, keepdim=True)
-        mu = (mu - mu_mean) / mu_std
+        # mu_mean = torch.mean(mu, 0, keepdim=True)
+        # mu_std = torch.std(mu, 0, keepdim=True)
+        # mu = (mu - mu_mean) / mu_std
 
         logvar = self.fc2(swin_output)
         # logvar_mean = torch.mean(logvar, 0, keepdim=True)
