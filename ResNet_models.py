16b316a7fb6c6e50cfd83b072b34b688e35ebe0e
tinu <tanveerkhattak3797@gmail.com>
2021-08-27 21:34:13 +0900

    updates

--- ResNet_models.py
+++ ResNet_models.py
@@ -100,14 +100,14 @@ class Encoder_x(nn.Module):
         # output = self.tanh(output)
 
         mu = self.fc1(swin_output)
-        mu_mean = torch.mean(mu, 0, keepdim=True)
-        mu_std = torch.std(mu, 0, keepdim=True)
-        mu = (mu - mu_mean) / mu_std
+        # mu_mean = torch.mean(mu, 0, keepdim=True)
+        # mu_std = torch.std(mu, 0, keepdim=True)
+        # mu = (mu - mu_mean) / mu_std
 
         logvar = self.fc2(swin_output)
-        logvar_mean = torch.mean(logvar, 0, keepdim=True)
-        log_std = torch.std(logvar, 0, keepdim=True)
-        logvar = (logvar - logvar_mean) / log_std
+        # logvar_mean = torch.mean(logvar, 0, keepdim=True)
+        # log_std = torch.std(logvar, 0, keepdim=True)
+        # logvar = (logvar - logvar_mean) / log_std
 
         dist = Independent(Normal(loc=mu, scale=torch.exp(logvar)), 1)
         # print(output.size())
@@ -160,10 +160,10 @@ class Encoder_xy(nn.Module):
         # output = self.leakyrelu(self.bn4(self.layer5(output)))
         # output = output.view(-1, 256 * 7 * 7)
 
-        copy = mu = self.fc1(swin_output)
-        mu_mean = torch.mean(mu, 0, keepdim=True)
-        mu_std = torch.std(mu, 0, keepdim=True)
-        mu = (mu - mu_mean) / mu_std
+        mu = self.fc1(swin_output)
+        # mu_mean = torch.mean(mu, 0, keepdim=True)
+        # mu_std = torch.std(mu, 0, keepdim=True)
+        # mu = (mu - mu_mean) / mu_std
 
         logvar = self.fc2(swin_output)
         logvar_mean = torch.mean(logvar, 0, keepdim=True)
