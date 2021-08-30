bf57bb7a22e6a13fcad256bb2c29401a5a0b9e13
tinu <tanveerkhattak3797@gmail.com>
2021-08-30 11:54:39 +0900

    updates

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 import pdb, os, argparse
-from ResNet_models_UCNet import Generator
+# from ResNet_models_UCNet import Generator
 from ResNet_models import Generator
 from data import test_dataset
 
