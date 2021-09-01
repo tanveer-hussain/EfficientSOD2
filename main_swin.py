376dda2c434f5196ed03a68427532997e451d7c1
tinu <tanveerkhattak3797@gmail.com>
2021-09-01 12:17:40 +0900

    updates

--- main_swin.py
+++ main_swin.py
@@ -612,8 +612,9 @@ class Segmentation(nn.Module):
 
         # Split image into overlapping patches from swin transformer class
         self.patch_embed = PatchEmbed(
-            img_size=img_size, p
-        )
+            img_size=img_size, patch_size=patch_size, in_chans=embed_dim, embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+
 
 
         
