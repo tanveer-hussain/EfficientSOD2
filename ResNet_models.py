d29d8dea4cb06f11a9e756e8437d481616761108
Tanveer Hussain <tanveerkhattak3797@gmail.com>
2021-09-02 15:33:29 +0900

    Update ResNet_models.py

--- ResNet_models.py
+++ ResNet_models.py
@@ -15,7 +15,7 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 swin_model = SwinTransformer()
-        
+checkpoint =        
 swin_model.to(device)
 print('parameters = ', count_parameters(swin_model))
 
