bec98d096f8593991e9315ad0ef8980c614b5e9d
tinu <tanveerkhattak3797@gmail.com>
2021-09-03 20:54:09 +0900

    updates

--- ResNet_models.py
+++ ResNet_models.py
@@ -125,8 +125,8 @@ class Encoder_xy(nn.Module):
         self.channel = channels
 
         # self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(channels * 8 * 11 * 11, latent_size)
-        self.fc2 = nn.Linear(channels * 8 * 11 * 11, latent_size)
+        self.fc1 = nn.Linear(256*7*7, latent_size)
+        self.fc2 = nn.Linear(256*7*7, latent_size)
         # self.fc1 = nn.Linear(49*768, latent_size)
         # self.fc2 = nn.Linear(49*768, latent_size)
 
