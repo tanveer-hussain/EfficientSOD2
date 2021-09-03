909f86f05a0873ef2cd9e155476f6d148e5d00a7
tinu <tanveerkhattak3797@gmail.com>
2021-09-03 19:03:13 +0900

    updates

--- main_Residual_swin.py
+++ main_Residual_swin.py
@@ -799,7 +799,7 @@ if __name__ == '__main__':
     img_size = height = width = 224
     model = SwinTransformer()
     print (model)
-    x = torch.randn((1,3,height,width))
+    x = torch.randn((16,3,height,width))
     x = model(x)
     print (x.shape)
 
