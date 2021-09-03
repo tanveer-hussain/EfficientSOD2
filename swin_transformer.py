b7938fc419bc87592d0f073ea685104ff13387b7
tinu <tanveerkhattak3797@gmail.com>
2021-09-03 19:29:57 +0900

    updates

--- swin_transformer.py
+++ swin_transformer.py
@@ -533,7 +533,7 @@ class SwinTransformer(nn.Module):
                                use_checkpoint=use_checkpoint)
             self.layers.append(layer)
 
-        # self.norm = norm_layer(self.num_features)
+        self.norm = norm_layer(self.num_features)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
         # self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
 
@@ -565,7 +565,7 @@ class SwinTransformer(nn.Module):
         for layer in self.layers:
             x = layer(x)
 
-        x = self.norm(x)  # B L C
+        # x = self.norm(x)  # B L C
         x = self.avgpool(x.transpose(1, 2))  # B C 1
         # x = torch.flatten(x, 1)
         return x
