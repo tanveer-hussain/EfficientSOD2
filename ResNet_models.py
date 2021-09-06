1965a2eecd6d9a0e3bc0b08774156c50a9c074ba
tinu <tanveerkhattak3797@gmail.com>
2021-09-06 18:15:17 +0900

    updates

--- ResNet_models.py
+++ ResNet_models.py
@@ -434,7 +434,7 @@ class Saliency_feat_encoder(nn.Module):
         self.custom_upsample = nn.Upsample(size=(224, 224), mode='bilinear', align_corners=True)
 
         self.conv1 = Triple_Conv(9, 3)
-        self.conv2 = Triple_Conv(512, channel)
+        self.convlast = Triple_Conv(3, 1)
         self.conv3 = Triple_Conv(1024, channel)
         self.conv4 = Triple_Conv(2048, channel)
 
@@ -500,9 +500,10 @@ class Saliency_feat_encoder(nn.Module):
         x = torch.cat((x, depth, z), 1)
         x = self.conv1(x)
 
-        # sal_init = swin_model(x)#.transpose(1,2)
+        sal_init = swin_model(x)#.transpose(1,2)
         # sal_init = self.patch_unembed(sal_init, x_size)
-        # depth_pred = swin_model(depth).transpose(1,2).unsqueeze(1)
+        depth_pred = swin_model(depth)
+        depth_pred = self.convlast(depth_pred)#.transpose(1,2).unsqueeze(1)
         # # sal_init = sal_init.transpose(1,2)
         # sal_init = self.upsample4(sal_init)
         # sal_init = self.custom_upsample(sal_init)
