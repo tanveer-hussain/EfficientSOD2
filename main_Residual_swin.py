b6e92f800dd39c133534f9280da91b598c42078a
tinu <tanveerkhattak3797@gmail.com>
2021-09-06 18:31:32 +0900

    updates

--- main_Residual_swin.py
+++ main_Residual_swin.py
@@ -795,24 +795,24 @@ class SwinIR(nn.Module):
         return flops
 
 from swin_transformer import  SwinTransformer
-if __name__ == '__main__':
-    # img_size = height = width = 224
-    # model = SwinTransformer()
-    # print (model)
-    # x = torch.randn((16,3,height,width))
-    # x = model(x)
-    # print (x.shape)
-
-    upscale = 4
-    window_size = 8
-    height = (1024 // upscale // window_size + 1) * window_size
-    width = (720 // upscale // window_size + 1) * window_size
-    model = SwinIR(upscale=2, img_size=(height, width),
-                   window_size=window_size, img_range=1., depths=[6, 6, 6, 6],
-                   embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2)
-    print(model)
-    print(height, width, model.flops() / 1e9)
-
-    x = torch.randn((1, 3, height, width))
-    x = model(x)
-    print(x.shape)
+# if __name__ == '__main__':
+#     # img_size = height = width = 224
+#     # model = SwinTransformer()
+#     # print (model)
+#     # x = torch.randn((16,3,height,width))
+#     # x = model(x)
+#     # print (x.shape)
+#
+#     upscale = 4
+#     window_size = 8
+#     height = (1024 // upscale // window_size + 1) * window_size
+#     width = (720 // upscale // window_size + 1) * window_size
+#     model = SwinIR(upscale=2, img_size=(height, width),
+#                    window_size=window_size, img_range=1., depths=[6, 6, 6, 6],
+#                    embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2)
+#     print(model)
+#     print(height, width, model.flops() / 1e9)
+#
+#     x = torch.randn((1, 3, height, width))
+#     x = model(x)
+#     print(x.shape)
