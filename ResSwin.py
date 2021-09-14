6ca6794d1ea9605c1db66d31ae12bed91de17eb5
tinu <tanveerkhattak3797@gmail.com>
2021-09-14 12:32:03 +0900

    Initial commit

--- ResSwin.py
+++ ResSwin.py
@@ -1,15 +1,16 @@
 import torch
-
+from  ResNet_models_Custom import Saliency_feat_encoder, Encoder_x, Encoder_xy
+import torch.nn as nn
 from ResNet_models_UCNet import Generator
 device = torch.device('cuda' if torch.cuda.is_available else "cpud")
-generator = Generator(32, 3)
-generator.cuda()
 
 class ResSwin():
-    def __init__(self):
+    def __init__(self, channel, latent_dim):
+        self.relu = nn.ReLU(inplace=True)
+        self.sal_encoder = Saliency_feat_encoder(channel, latent_dim)
+        self.xy_encoder = Encoder_xy(7, channel, latent_dim)
+        self.x_encoder = Encoder_x(6, channel, latent_dim)
 
 x = torch.randn((8, 3, 224, 224)).to(device)
 depth = torch.randn((8, 3, 224, 224)).to(device)
 gt = torch.randn((8, 1, 224, 224)).to(device)
-sal_init, depth_pred = generator(x,depth, gt)
-print(sal_init.shape, " > ", depth_pred.shape)
