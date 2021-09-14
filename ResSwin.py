a04fa00c0d40e98679bec40e89750a295fffe2c5
tinu <tanveerkhattak3797@gmail.com>
2021-09-14 12:34:12 +0900

    Initial commit

--- ResSwin.py
+++ ResSwin.py
@@ -11,6 +11,29 @@ class ResSwin():
         self.xy_encoder = Encoder_xy(7, channel, latent_dim)
         self.x_encoder = Encoder_x(6, channel, latent_dim)
 
+    def _make_pred_layer(self, block, dilation_series, padding_series, NoLabels, input_channel):
+        return block(dilation_series, padding_series, NoLabels, input_channel)
+
+    def kl_divergence(self, posterior_latent_space, prior_latent_space):
+        kl_div = kl.kl_divergence(posterior_latent_space, prior_latent_space)
+        return kl_div
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = torch.cuda.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x, depth, y=None, training=True):
+        if training:
+            self.posterior, muxy, logvarxy = self.xy_encoder(torch.cat((x,depth,y),1))
+            self.prior, mux, logvarx = self.x_encoder(torch.cat((x,depth),1))
+            lattent_loss = torch.mean(self.kl_divergence(self.posterior, self.prior))
+            z_noise_post = self.reparametrize(muxy, logvarxy)
+            z_noise_prior = self.reparametrize(mux, logvarx)
+            self.prob_pred_post, self.depth_pred_post  = self.sal_encoder(x,depth,z_noise_post)
+            self.prob_pred_prior, self.depth_pred_prior = self.sal_encoder(x, depth, z_noise_prior)
+
 x = torch.randn((8, 3, 224, 224)).to(device)
 depth = torch.randn((8, 3, 224, 224)).to(device)
 gt = torch.randn((8, 1, 224, 224)).to(device)
