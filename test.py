66c53aa29fac8d06f265f3e2742562b4890a7cce
tinu <tanveerkhattak3797@gmail.com>
2021-09-16 21:15:59 +0900

    Initial commit

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ for i in range(test_loader.size):
     import timeit
 
     start_time = timeit.default_timer()
-    generator_pred = generator.forward(image, depth, training=False)
+    generator_pred = resswin.forward(image, depth, training=False)
     #print('Single prediction time consumed >> , ', timeit.default_timer() - start_time, ' seconds')
     res = generator_pred
     res = F.upsample(res, size=[WW,HH], mode='bilinear', align_corners=False)
