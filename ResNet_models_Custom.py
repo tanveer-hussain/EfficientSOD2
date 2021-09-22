d40939b3eb62d414d460dce022943789e9b862e4
tinu <tanveerkhattak3797@gmail.com>
2021-09-23 06:30:41 +0900

    Initial commit

--- ResNet_models_Custom.py
+++ ResNet_models_Custom.py
@@ -211,8 +211,8 @@ class Saliency_feat_encoder(nn.Module):
     def __init__(self, channel, latent_dim):
         super(Saliency_feat_encoder, self).__init__()
 
-        # model_path = "/home/tinu/PycharmProjects/EfficientSOD2/swin_ir/002_lightweightSR_DIV2K_s64w8_SwinIR-S_x4.pth"
-        model_path = r"C:\Users\user02\Documents\GitHub\EfficientSOD2\002_lightweightSR_DIV2K_s64w8_SwinIR-S_x4.pth"
+        model_path = "/home/tinu/PycharmProjects/EfficientSOD2/swin_ir/002_lightweightSR_DIV2K_s64w8_SwinIR-S_x4.pth"
+        # model_path = r"C:\Users\user02\Documents\GitHub\EfficientSOD2\002_lightweightSR_DIV2K_s64w8_SwinIR-S_x4.pth"
         self.swinmodel = SwinIR(upscale=4, in_chans=3, img_size=64, window_size=8,
                                 img_range=1., depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6],
                                 mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')
