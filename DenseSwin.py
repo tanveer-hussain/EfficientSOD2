2cf745bdef1c06b0011e1c20977765d1af0fdbc9
tinu <tanveerkhattak3797@gmail.com>
2021-10-02 12:43:23 +0900

    Initial commit

--- DenseSwin.py
+++ DenseSwin.py
@@ -3,6 +3,39 @@ import torch.nn as nn
 device = torch.device('cuda' if torch.cuda.is_available else "cpu")
 from swin_transformer import *
 
+class PatchUnEmbed(nn.Module):
+    r""" Image to Patch Unembedding
+
+    Args:
+        img_size (int): Image size.  Default: 224.
+        patch_size (int): Patch token size. Default: 4.
+        in_chans (int): Number of input image channels. Default: 3.
+        embed_dim (int): Number of linear projection output channels. Default: 96.
+        norm_layer (nn.Module, optional): Normalization layer. Default: None
+    """
+
+    def __init__(self, img_size=224, patch_size=4, in_chans=3, embed_dim=96, norm_layer=None):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.patches_resolution = patches_resolution
+        self.num_patches = patches_resolution[0] * patches_resolution[1]
+
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+
+    def forward(self, x, x_size):
+        B, HW, C = x.shape
+        x = x.transpose(1, 2).view(B, self.embed_dim, x_size[0], x_size[1])  # B Ph*Pw C
+        return x
+
+    def flops(self):
+        flops = 0
+        return flops
+
 class DSTB(nn.Module):
     """Dense Swin Transformer Block (DSTB).
 
@@ -54,20 +87,128 @@ class DSTB(nn.Module):
                                       nn.Conv2d(dim // 4, dim, 3, 1, 1))
 
         self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=0, embed_dim=dim, norm_layer=None
+            img_size=img_size, patch_size=patch_size, in_chans=3, embed_dim=dim, norm_layer=None
         )
 
-        self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=0, embed_dim=dim,
+        self.patch_unembed = PatchUnEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=3, embed_dim=dim,
             norm_layer=None)
 
 
     def forward(self, x, x_size):
+        x = self.dense_group(x, x_size)
+        x = self.patch_unembed(x, x_size)
+        x = self.conv(x)
+        x = self.patch_embed(x) + x
 
+        return x
 
-# x = torch.randn((12, 3, 224, 224)).to(device)
+    def flops(self):
+        flops = 0
+        flops += self.dense_group.flops()
+        H, W = self.input_resolution
+        flops += H * W * self.dim * self.dim * 9
+        flops += self.patch_embed.flops()
+        flops += self.patch_unembed.flops()
+
+        return flops
+
+class SwinSaliency(nn.Module):
+    
+    
+    def __init__(self, img_size=224, patch_size=1, in_chans=3,
+                 embed_dim=96, depths=[6, 6, 6, 6], num_heads=[6, 6, 6, 6],
+                 window_size=7, mlp_ratio=4., qkv_bias=True, qk_scale=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
+                 norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
+                 use_checkpoint=False, upscale=2, img_range=1., upsampler='', dense_connection='1conv',
+                 **kwargs):
+        super(SwinSaliency, self).__init__()
+
+        ############################## Shallow features extraction ################################
+        self.conv_first = nn.Conv2d(in_chans, embed_dim, 3, 1, 1)
+
+        ############################## Deep features extraction ###################################
+        self.num_layers = len(depths)
+        self.embed_dim = embed_dim
+        self.patch_norm = patch_norm
+        self.num_features = embed_dim
+        self.mlp_ratio = mlp_ratio
+
+        # split image into non-overlapping patches
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=embed_dim, embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+        num_patches = self.patch_embed.num_patches
+        patches_resolution = self.patch_embed.patches_resolution
+        self.patches_resolution = patches_resolution
+
+        # merge non-overlapping patches into image
+        self.patch_unembed = PatchUnEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=embed_dim, embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+
+        # stochastic depth
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        # build dense siwn transformer blocks (DSTB)
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layer = DSTB(
+                dim=embed_dim,
+                input_resolution=(patches_resolution[0],
+                                  patches_resolution[1]),
+                depth=depths[i_layer],
+                num_heads=num_heads[i_layer],
+                window_size=window_size,
+                mlp_ratio=self.mlp_ratio,
+                qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate,
+                drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],  # no impact on SR results
+                norm_layer=norm_layer,
+                downsample=None,
+                use_checkpoint=use_checkpoint,
+                img_size=img_size,
+                patch_size=patch_size,
+                dense_connection=dense_connection
+            )
+            self.layers.append(layer)
+
+            # build the last conv layer in deep feature extraction
+        if dense_connection == '1conv':
+            self.conv_after_body = nn.Conv2d(embed_dim, embed_dim, 3, 1, 1)
+        elif dense_connection == '3conv':
+            # to save parameters and memory
+            self.conv_after_body = nn.Sequential(nn.Conv2d(embed_dim, embed_dim // 4, 3, 1, 1),
+                                                 nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                                                 nn.Conv2d(embed_dim // 4, embed_dim // 4, 1, 1, 0),
+                                                 nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                                                 nn.Conv2d(embed_dim // 4, embed_dim, 3, 1, 1))
+    def forward_features(self, x):
+        x_size = (x.shape[2], x.shape[3])
+        x = self.patch_embed(x)
+        x = self.pos_drop(x)
+        for layer in self.layers:
+            x = layer(x, x_size)
+        x = self.patch_unembed(x, x_size)
+
+        return x
+
+    def forward(self, x):
+        x = self.conv_first(x)
+        x = self.forward_features(x)
+        x = self.conv_after_body(x)
+
+        return x
+
+x = torch.randn((2, 3, 224, 224)).to(device)
 # depth = torch.randn((12, 3, 224, 224)).to(device)
 # gt = torch.randn((12, 1, 224, 224)).to(device)
-# model = ResSwin(32,3).to(device)
+model = SwinSaliency().to(device)
+x = model(x)
+print (model)
+
 # a, b, c, d, e = model(x,depth, gt)
 # print ('done')
