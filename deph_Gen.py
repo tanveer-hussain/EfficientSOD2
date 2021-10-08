b60efc4276b9dc0d28da611f277859ce35fa480e
tinu <tanveerkhattak3797@gmail.com>
2021-10-08 21:48:45 +0900

    Initial commit

--- deph_Gen.py
+++ deph_Gen.py
@@ -35,45 +35,45 @@ else:
     transform = midas_transforms.small_transform
 
 # create output folders for 8-bit simple output and 32-bit fully 3d vector output
-try:
-  os.makedirs('out8tif')
-except:
-  print('Folder not created')
-pp=1
-# for each filename do the depth extraction and save it
-for pp in range (pp>0):
-  # read file
-  img = cv2.imread('0001.jpg')
-  img=cv2.resize(img, (224,224))
-  # convert color space from BGR to RGB
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  # run midas model
-  input_batch = transform(img).to(device)
-  with torch.no_grad():
-    prediction = midas(input_batch)
-
-    prediction = torch.nn.functional.interpolate(
-        prediction.unsqueeze(1),
-        size=img.shape[:2],
-        mode="bicubic",
-        align_corners=False,
-    ).squeeze()
-  # convert output to numpy array
-  output = prediction.cpu().numpy()
-
-  # rescale output for simple depth extraction
-  min = np.min(output)
-  max = np.max(output)
-  output2 = 255.99*(output-min)/(max-min)
-  output2 = output2.astype(int)
-  output2 = np.stack((output2,)*3, axis=-1)
-  img1=output2.astype(np.uint8)
-  # save simple output together with blurred versions
-  cv2.imwrite('out8tif/1.jpg', img1)
-  
-cv2.imshow('sample image',img1)
-cv2.waitKey(0) # waits until a key is pressed
-cv2.destroyAllWindows()
+# try:
+#   os.makedirs('out8tif')
+# except:
+#   print('Folder not created')
+# pp=1
+# # for each filename do the depth extraction and save it
+# for pp in range (pp>0):
+#   # read file
+#   img = cv2.imread('0001.jpg')
+#   img=cv2.resize(img, (224,224))
+#   # convert color space from BGR to RGB
+#   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#   # run midas model
+#   input_batch = transform(img).to(device)
+#   with torch.no_grad():
+#     prediction = midas(input_batch)
+#
+#     prediction = torch.nn.functional.interpolate(
+#         prediction.unsqueeze(1),
+#         size=img.shape[:2],
+#         mode="bicubic",
+#         align_corners=False,
+#     ).squeeze()
+#   # convert output to numpy array
+#   output = prediction.cpu().numpy()
+#
+#   # rescale output for simple depth extraction
+#   min = np.min(output)
+#   max = np.max(output)
+#   output2 = 255.99*(output-min)/(max-min)
+#   output2 = output2.astype(int)
+#   output2 = np.stack((output2,)*3, axis=-1)
+#   img1=output2.astype(np.uint8)
+#   # save simple output together with blurred versions
+#   cv2.imwrite('out8tif/1.jpg', img1)
+#
+# cv2.imshow('sample image',img1)
+# cv2.waitKey(0) # waits until a key is pressed
+# cv2.destroyAllWindows()
 
 def return_depth(img):
     img = cv2.resize(img, (224, 224))
@@ -103,8 +103,8 @@ def return_depth(img):
     return depth
 
 
-source_directory = r""
-destin_directory = r""
+source_directory = r"C:\Users\khank\PycharmProjects\EfficientSOD2\Input"
+destin_directory = r"C:\Users\khank\PycharmProjects\EfficientSOD2\out8tif"
 for image_name in os.listdir(source_directory):
     print (f'Processing.. *{source_directory,image_name}*')
     single_image = cv2.imread(os.path.join(source_directory,image_name))
