9f79379d93511cc00261772bd44cdcebc5fb07b2
tinu <tanveerkhattak3797@gmail.com>
2021-10-08 22:56:18 +0900

    Initial commit

--- data.py
+++ data.py
@@ -4,42 +4,56 @@ import cv2
 import torch
 import numpy as np
 import torch.utils.data as data
-import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as T
 
+class DatasetLoader(Dataset):
 
-class SalObjDataset(data.Dataset):
-    def __init__(self, image_root, gt_root, depth_root, gray_root, trainsize):
-        self.trainsize = trainsize
-        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg')]
-        self.gts = [gt_root + f for f in os.listdir(gt_root) if f.endswith('.jpg')
-                    or f.endswith('.png')]
-        # self.depths = [depth_root + f for f in os.listdir(depth_root) if f.endswith('.png')]
-        self.grays = [gray_root + f for f in os.listdir(gray_root) if f.endswith('.jpg')]
-        self.images = sorted(self.images)
-        self.gts = sorted(self.gts)
-        # self.depths = sorted(self.depths)
-        self.grays = sorted(self.grays)
-        self.filter_files()
-        self.size = len(self.images)
-        self.img_transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        self.gt_transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor()])
-        self.depth_transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor()])
-        self.gray_transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor()])
+    def __init__(self, dir, d_type):
+
+        self.x_path = os.path.join(dir, str(d_type), 'Images')
+        self.y_path = os.path.join(dir, str(d_type), 'Labels')
+
+        self.X = os.listdir(self.x_path)
+        self.Y = os.listdir(self.y_path)
+
+        self.length = len(self.X)
+
+        self.img_transform = T.Compose([
+            T.Resize((224, 224)),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        self.gt_transform = T.Compose([
+            T.Resize((224, 224)),
+            T.ToTensor()])
 
         ###################### depth estimation
         self.midas = torch.hub.load("intel-isl/MiDaS", "MiDaS")
         self.midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
         self.midas.eval()
         self.transform = self.midas_transforms.default_transform
+
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        x_full_path = os.path.join(self.x_path, self.X[index])
+        y_full_path = os.path.join(self.y_path, self.Y[index])
+
+        x = Image.open(x_full_path).convert('RGB')
+        y = Image.open(y_full_path).convert('L')
+
+
+        x = self.img_transform(x)
+        y = self.gt_transform(y)
+        d = self.gt_transform(d)
+
+
+        return x , y
+
+
+
 
     def return_depth(self, img):
         img = cv2.resize(img, (224, 224))
@@ -69,125 +83,4 @@ class SalObjDataset(data.Dataset):
         pil_depth = Image.fromarray(depth)
         return pil_depth
 
-    def __getitem__(self, index):
-        image = self.rgb_loader(self.images[index])
-        gt = self.binary_loader(self.gts[index])
-        depth = self.return_depth(np.asarray(image))
-        # depth = self.rgb_loader(self.depths[index])
-        gray = self.binary_loader(self.grays[index])
-        image = self.img_transform(image)
-        gt = self.gt_transform(gt)
 
-        depth = self.depth_transform(depth)
-        gray = self.gray_transform(gray)
-        # img_names = self.images[index]
-        return image, gt, depth, gray, index
-
-    def filter_files(self):
-        assert len(self.images) == len(self.gts)
-        assert len(self.images) == len(self.depths)
-        assert len(self.images) == len(self.grays)
-        images = []
-        gts = []
-        depths = []
-        grays = []
-        for img_path, gt_path, depth_path, gray_path in zip(self.images, self.gts, self.depths, self.grays):
-            img = Image.open(img_path)
-            gt = Image.open(gt_path)
-            depth = Image.open(depth_path)
-            gray = Image.open(gray_path)
-            if img.size == gt.size:
-                images.append(img_path)
-                gts.append(gt_path)
-                depths.append(depth_path)
-                grays.append(gray_path)
-        self.images = images
-        self.gts = gts
-        self.depths = depths
-        self.grays = grays
-
-    def rgb_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('RGB')
-
-    def binary_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            # return img.convert('1')
-            return img.convert('L')
-
-    def resize(self, img, gt):
-        assert img.size == gt.size
-        w, h = img.size
-        if h < self.trainsize or w < self.trainsize:
-            h = max(h, self.trainsize)
-            w = max(w, self.trainsize)
-            return img.resize((w, h), Image.BILINEAR), gt.resize((w, h), Image.NEAREST)
-        else:
-            return img, gt
-
-    def __len__(self):
-        return self.size
-
-
-def get_loader(image_root, gt_root, depth_root, gray_root, batchsize, trainsize, shuffle=True, num_workers=12, pin_memory=True):
-
-    dataset = SalObjDataset(image_root, gt_root, depth_root, gray_root, trainsize)
-    data_loader = data.DataLoader(dataset=dataset,
-                                  batch_size=batchsize,
-                                  shuffle=shuffle,
-                                  num_workers=num_workers,
-                                  pin_memory=pin_memory)
-    return data_loader, dataset.size
-
-# def update_data_loader(new_z, dataset):
-#
-#     dataset = SalObjDataset(image_root, gt_root, trainsize)
-#     data_loader = data.DataLoader(dataset=dataset,
-#                                   batch_size=batchsize,
-#                                   shuffle=shuffle,
-#                                   num_workers=num_workers,
-#                                   pin_memory=pin_memory)
-#     return data_loader
-
-class test_dataset:
-    def __init__(self, image_root, depth_root, testsize):
-        self.testsize = testsize
-        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
-        self.depths = [depth_root + f for f in os.listdir(depth_root) if f.endswith('.bmp')
-                    or f.endswith('.png')]
-        self.images = sorted(self.images)
-        self.depths = sorted(self.depths)
-        self.transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        self.depth_transform = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.ToTensor()])
-        self.size = len(self.images)
-        self.index = 0
-
-    def load_data(self):
-        image = self.rgb_loader(self.images[self.index])
-        depth = self.rgb_loader(self.depths[self.index])
-        HH = 224
-        WW = 224
-        image = self.transform(image).unsqueeze(0)
-        depth = self.depth_transform(depth).unsqueeze(0)
-        name = self.images[self.index].split('/')[-1]
-        if name.endswith('.jpg'):
-            name = name.split('.jpg')[0] + '.png'
-        self.index += 1
-        return image, depth, HH, WW, name
-
-    def rgb_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('RGB')
-
-    def binary_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('L')
--- train.py
+++ train.py
@@ -37,7 +37,7 @@ parser.add_argument('--depth_loss_weight', type=float, default=0.1, help='weight
 
 opt = parser.parse_args()
 print('Generator Learning Rate: {}'.format(opt.lr_gen))
-torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 from ResSwin import ResSwinModel
 resswin = ResSwinModel(channel=opt.feat_channel, latent_dim=opt.latent_dim)
 resswin.to(device)
