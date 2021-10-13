2f67bd948f977ef71d77328e4b0b88bbb6e1be14
tinu <tanveerkhattak3797@gmail.com>
2021-10-14 02:46:23 +0900

    Initial commit

--- ResNet_models_Custom.py
+++ ResNet_models_Custom.py
@@ -358,21 +358,33 @@ class Saliency_feat_encoder(nn.Module):
         # depth_pred = self.conv4_depth1(depth_pred)
 
         # conv1_feat = self.conv1(x1)
-        for layer in self.b1_layers:
-            conv1_feat = layer(x1)
+        conv1_feat = self.b1_layers[0](x1)
+        conv1_feat = self.b1_layers[1](conv1_feat)
+        conv1_feat = self.b1_layers[2](conv1_feat)
+        conv1_feat = self.b1_layers[3](conv1_feat)
+
+        conv2_feat = self.b2_layers[0](x2)
+        conv2_feat = self.b2_layers[1](conv2_feat)
+        conv2_feat = self.b2_layers[2](conv2_feat)
+        conv2_feat = self.b2_layers[3](conv2_feat)
+
+        conv3_feat = self.b3_layers[0](x3)
+        conv3_feat = self.b3_layers[1](conv3_feat)
+        conv3_feat = self.b3_layers[2](conv3_feat)
+        conv3_feat = self.b3_layers[3](conv3_feat)
+
+        conv4_feat = self.b4_layers[0](x4)
+        conv4_feat = self.b4_layers[1](conv4_feat)
+        conv4_feat = self.b4_layers[2](conv4_feat)
+        conv4_feat = self.b4_layers[3](conv4_feat)
+
 
         # conv1_feat = self.asppconv1(conv1_feat)
         # conv2_feat = self.conv2(x2)
-        for layer in self.b2_layers:
-            conv2_feat = layer(x2)
         # conv2_feat = self.asppconv2(conv2_feat)
         # conv3_feat = self.conv3(x3)
-        for layer in self.b3_layers:
-            conv3_feat = layer(x3)
         # conv3_feat = self.asppconv3(conv3_feat)
         # conv4_feat = self.conv4(x4)
-        for layer in self.b4_layers:
-            conv4_feat = layer(x4)
         # conv4_feat = self.asppconv4(conv4_feat)
         conv4_feat = self.upsample2(conv4_feat)
 
