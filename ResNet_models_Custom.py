5d22a1e5279d77f3654739bc2281bcac6113bf59
tinu <tanveerkhattak3797@gmail.com>
2021-10-17 17:40:58 +0900

    Initial commit

--- ResNet_models_Custom.py
+++ ResNet_models_Custom.py
@@ -257,11 +257,11 @@ class Saliency_feat_encoder(nn.Module):
         self.aspp_mhsa1_2 = Pyramid_block(32, 56, 32, 56, 4, 2)
 
         self.aspp_mhsa2_1 = Pyramid_block(32,28,32,28,4,1)
-        # self.aspp_mhsa2_2 = Pyramid_block(256, 28, 64, 28, 4, 2)
+        self.aspp_mhsa2_2 = Pyramid_block(32, 28, 32, 28, 4, 2)
         # self.aspp_mhsa2_3 = Pyramid_block(64, 28, 32, 28, 4, 3)
 
         self.aspp_mhsa3_1 = Pyramid_block(32, 14, 32, 14, 4, 1)
-        # self.aspp_mhsa3_2 = Pyramid_block(512, 14, 128, 14, 4, 2)
+        self.aspp_mhsa3_2 = Pyramid_block(32, 14, 32, 14, 4, 2)
         # self.aspp_mhsa3_3 = Pyramid_block(128, 14, 32, 14, 4, 3)
 
         self.aspp_mhsa4 = Pyramid_block(32, 7, 32, 7, 4, 2)
