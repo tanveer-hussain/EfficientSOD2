00bbe4c661f85a4fce1cc331d4980618abe6c7ce
Tanveer Hussain <tanveerkhattak3797@gmail.com>
2021-10-19 01:32:31 +0900

    Initial commit

--- data.py
+++ data.py
@@ -64,58 +64,30 @@ from ResSwin import ResSwinModel
 device = torch.device('cuda' if torch.cuda.is_available else "cpu")
 class TestDatasetLoader(data.Dataset):
 
-    def __init__(self, dir, d_type, weights_path):
-        self.x_path = os.path.join(dir, str(d_type), 'Images')
-        self.y_path = os.path.join(dir, str(d_type), 'Labels')
+    def __init__(self, image_root, label_root):
+        self.x_path = image_root
+        self.y_path = label_root
 
         self.X = os.listdir(self.x_path)
         self.Y = os.listdir(self.y_path)
 
-        self.resswin = ResSwinModel(channel=32, latent_dim=3)
-        self.resswin.to(device)
-        print(self.resswin.load_state_dict(torch.load(weights_path)))
-        # self.D = os.listdir(self.d_path)
-
         self.length = len(self.X)
-
-        self.img_transform = T.Compose([
-            T.Resize((224, 224)),
-            T.ToTensor(),
-            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        self.gt_transform = T.Compose([T.ToTensor()])
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, index):
-        x_full_path = os.path.join(self.x_path, self.X[index])
-        y_full_path = os.path.join(self.y_path, self.Y[index])
+        pred_full_path = os.path.join(self.x_path, self.X[index])
+        gt_full_path = os.path.join(self.y_path, self.Y[index])
+
+        pred = Image.open(pred_full_path).convert('L')
+        gt = Image.open(gt_full_path).convert('L')
+
+        if pred.size != gt.size:
+            pred = pred.reshape(gt.size, Image.BILINEAR)
 
 
-        # d_full_path = os.path.join(self.d_path, self.D[index])
-
-        x = Image.open(x_full_path).convert('RGB')
-        y = Image.open(y_full_path).convert('L')
-        x = self.img_transform(x)
-
-        pred = self.resswin.forward(x, training=False)
-
-        pred = F.interpolate(pred, size=(x.shape[1], x.shape[2]), mode='bilinear', align_corners=False)
-        pred = torch.squeeze(pred, 0)
-        pred = pred.detach().cpu().numpy()
-        pred = pred.dot(255)
-        pred *= pred.max() / 255.0
-        pred = np.transpose(pred, (1, 2, 0))
-
-
-
-
-        y = np.array(y)
-
-        print(pred.shape, "and ", y.shape)
-
-
-        return pred , y
+        return pred , gt
 
 
 class test_dataset:
