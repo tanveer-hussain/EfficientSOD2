16569858482ae3eafbf7068dacd71722514361b4
tinu <tanveerkhattak3797@gmail.com>
2021-10-29 02:08:48 +0900

    Initial commit

--- ResSwin.py
+++ ResSwin.py
@@ -91,24 +91,6 @@ class ResSwinModel(nn.Module):
         # # self.aspp_mhsa3_2 = Pyramid_block(32, 14, 32, 14, 4, 2)
         #
         self.aspp_mhsa4 = Pyramid_block(32, 14, 32, 14, 4, 1)
-        features = 256
-        non_negative = True
-
-        self.head_depth = nn.Sequential(
-            nn.Conv2d(features, features // 2, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(features // 2, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(32, 32, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(True) if non_negative else nn.Identity(),
-            nn.Identity(),
-        )
-        self.head_seg = nn.Sequential(
-            nn.Conv2d(features, features, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(features),
-            nn.ReLU(True),
-            nn.Dropout(0.1, False),
-            nn.Conv2d(features, channel, kernel_size=1),
-        )
 
 
         # self.sal_encoder = Saliency_feat_encoder(channel, latent_dim)
@@ -123,15 +105,13 @@ class ResSwinModel(nn.Module):
         self.conv432 = Triple_Conv(3 * channel, channel)
         self.conv4321 = Triple_Conv(4 * channel, channel)
         self.conv1_1 = Triple_Conv(64, channel)
-        self.conv1_11 = Triple_Conv(96, channel)
         self.conv1 = Triple_Conv(256, channel)
         self.layer6 = self._make_pred_layer(Classifier_Module, [6, 12, 18, 24], [6, 12, 18, 24], 1, channel)
         self.upsample8 = nn.Upsample(scale_factor=8, mode='bilinear', align_corners=True)
         self.upsample4 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
         self.upsample2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         self.conv2 = Triple_Conv(150, 1)
-        self.conv11 = Triple_Conv(32,1)
-        self.conv_depth_pred = Triple_Conv(32,1)
+        self.conv11 = Triple_Conv(6,3)
         # self.conv3 = Triple_Conv(1024, channel)
         # self.conv4 = Triple_Conv(2048, channel)
         # self.liner1024 = nn.Linear(2048, 1024)
@@ -148,21 +128,10 @@ class ResSwinModel(nn.Module):
         # x = torch.cat((x,d),1)
         # x = self.conv11(x)
         _, p1, p2, p3, p4 = self.dpt_model(x)
-        _, _, _, _, d4 = self.dpt_depth_model(d)
-        # p1 = self.head_seg(p1)
-        # p2 = self.head_seg(p2)
-        # p3 = self.head_seg(p3)
-        # p4 = self.head_seg(p4)
-        # d1 = self.head(d1)
-        # d2 = self.head(d2)
-        # d3 = self.head(d3)
-        d4 = self.head_depth(d4)
-
         # d1, d2, d3 = self.depth_model(d)
         # self.x1, self.x2, self.x3, self.x4 = self.sal_encoder(x, self.depth)
 
         conv1_feat = self.conv1(p1)
-        # d1 = F.interpolate(d1, size=(56,56), mode='bilinear', align_corners=True)
         conv1_feat_x1 = F.interpolate(conv1_feat, size=(56, 56), mode='bilinear', align_corners=True)
         # conv1_feat_x1_d1 = self.conv1_1(torch.cat((conv1_feat_x1,d1),1))
         conv1_feat = self.aspp_mhsa1(conv1_feat_x1)
@@ -177,10 +146,8 @@ class ResSwinModel(nn.Module):
         conv3_feat = self.aspp_mhsa3(conv3_feat_x3)
         conv3_feat = self.conv1_1(torch.cat((conv3_feat, conv3_feat_x3), 1))
         # conv3_feat = self.asppconv3(conv3_feat)
-        conv4_feat_x4 = self.conv1(p4)
-        # d4 = self.conv1(d4)
-        conv4_feat = self.aspp_mhsa4(conv4_feat_x4)
-        conv4_feat = self.conv1_11(torch.cat((conv4_feat,d4,conv4_feat_x4),1))
+        conv4_feat = self.conv1(p4)
+        conv4_feat = self.aspp_mhsa4(conv4_feat)
         # conv4_feat = self.asppconv4(conv4_feat)
         conv4_feat = self.upsample2(conv4_feat)
 
@@ -194,7 +161,8 @@ class ResSwinModel(nn.Module):
         conv432 = self.conv432(conv432)
 
         conv432 = self.upsample2(conv432)
-        conv4321 = torch.cat((self.upsample4(conv4_feat), self.upsample2(conv43), conv432, self.upsample2(conv1_feat)),1)
+        conv4321 = torch.cat((self.upsample4(conv4_feat), self.upsample2(conv43), conv432, self.upsample2(conv1_feat)),
+                             1)
         conv4321 = self.racb_4321(conv4321)
         conv4321 = self.conv4321(conv4321)
 
