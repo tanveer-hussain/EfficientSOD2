339656b48420025519384fa60100291baf5449cc
Tanveer Hussain <tanveerkhattak3797@gmail.com>
2022-06-23 12:18:50 +0900

    Initial commit

--- customlosses.py
+++ customlosses.py
@@ -50,3 +50,14 @@ def ssim(img1, img2, val_range, window_size=11, window=None, size_average=True, 
         return ret, cs
 
     return ret
+
+def structure_loss(pred, mask):
+    weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
+    wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
+
+    pred  = torch.sigmoid(pred)
+    inter = ((pred*mask)*weit).sum(dim=(2,3))
+    union = ((pred+mask)*weit).sum(dim=(2,3))
+    wiou  = 1-(inter+1)/(union-inter+1)
+    return (wbce+wiou).sum()
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from utils import l2_regularisation
 import smoothness
 import imageio
 import torch.nn as nn
-from customlosses import ssim
+from customlosses import ssim, structure_loss
 from torch.utils.data import DataLoader
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
@@ -22,18 +22,6 @@ CE = torch.nn.BCELoss()
 mse_loss = torch.nn.MSELoss(size_average=True, reduce=True)
 smooth_loss = smoothness.smoothness_loss(size_average=True)
 l1_criterion = nn.L1Loss()
-
-def structure_loss(pred, mask):
-    weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
-    wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
-    wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
-
-    pred  = torch.sigmoid(pred)
-    inter = ((pred*mask)*weit).sum(dim=(2,3))
-    union = ((pred+mask)*weit).sum(dim=(2,3))
-    wiou  = 1-(inter+1)/(union-inter+1)
-    return (wbce+wiou).sum()
-
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
